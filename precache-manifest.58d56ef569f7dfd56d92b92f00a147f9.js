693cb17768559d4baeec306c7bda55a1045c8e07
chauhandharmik <68230982+chauhandharmik@users.noreply.github.com>
2020-07-23 14:18:41 +0530

    Updates

---
+++ precache-manifest.58d56ef569f7dfd56d92b92f00a147f9.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9f51ce95c231bd0b88f2372eb82f0ff",
+    "url": "/react-live/index.html"
+  },
+  {
+    "revision": "2a535337a20e3474e41d",
+    "url": "/react-live/static/css/2.75c4ecca.chunk.css"
+  },
+  {
+    "revision": "e071d90e43e06108ddbf",
+    "url": "/react-live/static/css/main.11ab2ba5.chunk.css"
+  },
+  {
+    "revision": "2a535337a20e3474e41d",
+    "url": "/react-live/static/js/2.2d33ed4b.chunk.js"
+  },
+  {
+    "revision": "361388382a7cba0b7cd58ae9b81faa0e",
+    "url": "/react-live/static/js/2.2d33ed4b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e071d90e43e06108ddbf",
+    "url": "/react-live/static/js/main.7020a699.chunk.js"
+  },
+  {
+    "revision": "d6f6a2218f4c004f2518",
+    "url": "/react-live/static/js/runtime-main.ee193817.js"
+  },
+  {
+    "revision": "eabb513f45b4081e30318559b6608b31",
+    "url": "/react-live/static/media/1.eabb513f.png"
+  },
+  {
+    "revision": "ccc2b99031b808fb32f279235da85b65",
+    "url": "/react-live/static/media/2.ccc2b990.png"
+  },
+  {
+    "revision": "452a301d0b07d28056b956ebad1ce8f5",
+    "url": "/react-live/static/media/about.452a301d.jpg"
+  },
+  {
+    "revision": "6ddde3a9affef5da2fd86f1a2aff3516",
+    "url": "/react-live/static/media/digitalmarketing.6ddde3a9.jpg"
+  },
+  {
+    "revision": "28a93410cdb3575561305947907687f5",
+    "url": "/react-live/static/media/responnsive.28a93410.jpg"
+  },
+  {
+    "revision": "f763240e3a103c4332b5f8ec977b9207",
+    "url": "/react-live/static/media/software.f763240e.jpg"
+  },
+  {
+    "revision": "06c36c532a463757f043e397743d1bbe",
+    "url": "/react-live/static/media/webcoimbatore.06c36c53.png"
+  }
+]);
